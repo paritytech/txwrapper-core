d7c8880f2960689f10b5bc72a1c10706d3c38d54
Tarik <tariksnow37@gmail.com>
2021-01-07 15:20:53 -0500

    Rebond and test suites

---
+++ packages/txwrapper-substrate/src/methods/staking/rebond.spec.ts
@@ -0,0 +1,20 @@
+import {
+    TEST_BASE_TX_INFO,
+    POLKADOT_25_TEST_OPTIONS,
+    itHasCorrectBaseTxInfo,
+} from '@substrate/txwrapper-core';
+import { TEST_METHOD_ARGS } from '../../test-helpers';
+import { rebond } from './rebond';
+
+describe('staking::rebond', () => {
+    it('should work', () => {
+        const unsigned = rebond(
+            TEST_METHOD_ARGS.staking.rebond,
+            TEST_BASE_TX_INFO,
+            POLKADOT_25_TEST_OPTIONS
+        );
+
+        itHasCorrectBaseTxInfo(unsigned);
+        expect(unsigned.method).toBe('0x08139101');
+    });
+});
