3fba657a97c3106a969fe2b48943add5f1259660
Tarik <tariksnow37@gmail.com>
2021-01-07 13:41:22 -0500

    BondExtra, and test

---
+++ packages/txwrapper-substrate/src/methods/staking/bondExtra.spec.ts
@@ -0,0 +1,22 @@
+import {
+    POLKADOT_25_TEST_OPTIONS,
+    TEST_BASE_TX_INFO,
+    itHasCorrectBaseTxInfo,
+} from '@substrate/txwrapper-core';
+
+import { TEST_METHOD_ARGS } from '../../test-helpers';
+
+import { bondExtra } from './bondExtra';
+
+describe('staking::bondExtra', () => {
+    it('should encode', () => {
+        const unsigned = bondExtra(
+            TEST_METHOD_ARGS.staking.bondExtra,
+            TEST_BASE_TX_INFO,
+            POLKADOT_25_TEST_OPTIONS
+        );
+
+        itHasCorrectBaseTxInfo(unsigned);
+        expect(unsigned.method).toBe('0x08019101');
+    });
+});
