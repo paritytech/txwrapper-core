e02bb77f64fad3d4edabd7c0dffbd25ceebf3595
Tarik <tariksnow37@gmail.com>
2021-01-07 12:39:07 -0500

    Add Bond and test specs

---
+++ packages/txwrapper-substrate/src/methods/staking/bond.spec.ts
@@ -0,0 +1,37 @@
+import {
+    TEST_BASE_TX_INFO,
+} from '@substrate/txwrapper-core';
+
+import { testBaseTxInfo, KUSAMA_TEST_OPTIONS } from '../../util/testUtil'; 
+import { TEST_METHOD_ARGS } from '../../test-helpers';
+import { bond } from './bond';
+
+describe('staking::bond', () => {
+    it('should work', () => {
+        const unsigned = bond(
+            TEST_METHOD_ARGS.staking.bond,
+            TEST_BASE_TX_INFO,
+            KUSAMA_TEST_OPTIONS
+        );
+
+        testBaseTxInfo(unsigned);
+        expect(unsigned.method).toBe(
+            '0x07008eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48910100'
+        );
+    });
+
+    it('should take "staked" or "Staked" as payee', () => {
+        const unsignedLowerCase = bond(
+            { ...TEST_METHOD_ARGS.staking.bond, payee: 'staked' },
+            TEST_BASE_TX_INFO,
+            KUSAMA_TEST_OPTIONS
+        );
+        const unsignedCapitalized = bond(
+            { ...TEST_METHOD_ARGS.staking.bond, payee: 'Staked' },
+            TEST_BASE_TX_INFO,
+            KUSAMA_TEST_OPTIONS
+        );
+
+        expect(unsignedLowerCase.method).toBe(unsignedCapitalized.method);
+    });
+});
