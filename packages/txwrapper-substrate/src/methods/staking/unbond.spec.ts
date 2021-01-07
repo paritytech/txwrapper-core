29281f6db93548174d04f06f7c9dd64504fcd5ce
Tarik <tariksnow37@gmail.com>
2021-01-07 15:57:59 -0500

    validate and test suites

---
+++ packages/txwrapper-substrate/src/methods/staking/unbond.spec.ts
@@ -0,0 +1,20 @@
+import {
+    POLKADOT_25_TEST_OPTIONS,
+    TEST_BASE_TX_INFO,
+    itHasCorrectBaseTxInfo,
+} from '@substrate/txwrapper-core';
+import { TEST_METHOD_ARGS } from '../../test-helpers';
+import { unbond } from './unbond';
+
+describe('staking::unbond', () => {
+    it('should work', () => {
+        const unsigned = unbond(
+            TEST_METHOD_ARGS.staking.unbond,
+            TEST_BASE_TX_INFO,
+            POLKADOT_25_TEST_OPTIONS
+        );
+
+        itHasCorrectBaseTxInfo(unsigned);
+        expect(unsigned.method).toBe('0x08029101');
+    });
+});
