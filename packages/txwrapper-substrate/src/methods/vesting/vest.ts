edb5cd082c64dd89f5a924d4f174f4ddceae4d83
tarikgul <tarik@parity.io>
2021-02-02 23:24:48 -0500

    feat: add vesting methods

---
+++ packages/txwrapper-substrate/src/methods/vesting/vest.ts
@@ -0,0 +1,31 @@
+import {
+    BaseTxInfo,
+    defineMethod,
+    OptionsWithMeta,
+    UnsignedTransaction,
+} from '@substrate/txwrapper-core';
+
+/**
+ * Unlock any vested funds of the sender account.
+ *
+ * @param args - Arguments specific to this method.
+ * @param info - Information required to construct the transaction.
+ * @param options - Registry and metadata used for constructing the method.
+ */
+export function vest(
+    args: {},
+    info: BaseTxInfo,
+    options: OptionsWithMeta
+): UnsignedTransaction {
+    return defineMethod(
+        {
+            method: {
+                args,
+                name: 'vest',
+                pallet: 'vesting',
+            },
+            ...info,
+        },
+        options
+    );
+}
---
+++ packages/txwrapper-substrate/src/methods/vesting/vestOther.ts
@@ -0,0 +1,40 @@
+import {
+    Args,
+    BaseTxInfo,
+    defineMethod,
+    OptionsWithMeta,
+    UnsignedTransaction,
+} from '@substrate/txwrapper-core';
+
+export interface VestingVestOtherArgs extends Args {
+    /**
+     * The account whose vested funds should be unlocked. Must have funds still
+     * locked under this module.
+     */
+    target: string;
+}
+
+/**
+ * Unlock any vested funds of a `target` account.
+ *
+ * @param args - Arguments specific to this method.
+ * @param info - Information required to construct the transaction.
+ * @param options - Registry and metadata used for constructing the method.
+ */
+export function vestOther(
+    args: VestingVestOtherArgs,
+    info: BaseTxInfo,
+    options: OptionsWithMeta
+): UnsignedTransaction {
+    return defineMethod(
+        {
+            method: {
+                args,
+                name: 'vestOther',
+                pallet: 'vesting',
+            },
+            ...info,
+        },
+        options
+    );
+}
