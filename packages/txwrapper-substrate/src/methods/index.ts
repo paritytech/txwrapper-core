ec9910c62e063c603457a055748819032a467a1a
tarikgul <tarik@parity.io>
2021-02-02 23:29:00 -0500

    feat: methods entry file

--- packages/txwrapper-substrate/src/methods/index.ts
+++ packages/txwrapper-substrate/src/methods/index.ts
@@ -1,5 +1,6 @@
 // Name exports to create namespaces that map to pallets
 export * as balances from './balances';
+export * as democracy from './democracy';
 export * as proxy from './proxy';
 export * as staking from './staking';
 export * as utility from './utility';
