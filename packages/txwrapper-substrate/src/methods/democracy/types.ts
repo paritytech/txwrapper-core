7a03a54e7c0ddeeea34ccc9d003b2ed38970e77f
Tarik Gul <47201679+TarikGul@users.noreply.github.com>
2021-02-04 17:41:57 -0500

    Update packages/txwrapper-substrate/src/methods/democracy/types.ts
    Co-authored-by: David <dvdplm@gmail.com>

--- packages/txwrapper-substrate/src/methods/democracy/types.ts
+++ packages/txwrapper-substrate/src/methods/democracy/types.ts
@@ -21,7 +21,10 @@ type Vote = {
  * - Standard: A standard vote, one-way (approve or reject) with a given amount
  * of conviction.
  * - Split: A split vote with balances given for both ways, and with no
- * conviction, useful for parachains when voting.
+ * conviction. This is useful for parachains, which vote on behalf of their 
+participants and are free to choose their own way of splitting up the total
+balance. The stake behind a parachain can e.g. be split e.g. 2/3 aye and
+1/3 nay.
  */
 
 export type AccountVote =
