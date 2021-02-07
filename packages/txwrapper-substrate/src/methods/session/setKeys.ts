f2a1c063863e172bb42585255ea3b22be05d9f7a
tarikgul <tarik@parity.io>
2021-02-06 23:50:53 -0500

    feat: migrate system methods

---
+++ packages/txwrapper-substrate/src/methods/session/setKeys.ts
@@ -0,0 +1,48 @@
+import {
+    Args,
+    BaseTxInfo,
+    defineMethod,
+    OptionsWithMeta,
+    UnsignedTransaction,
+} from '@substrate/txwrapper-core';
+
+export interface SessionSetKeysArgs extends Args {
+    /**
+     * The 5 keys to set.
+     */
+    keys: string[];
+    /**
+     * Proof (unused for now).
+     */
+    proof?: string;
+}
+
+/**
+ * Sets the session key(s) of the function caller to `key`.
+ *
+ * @param args - Arguments specific to this method.
+ * @param info - Information required to construct the transaction.
+ * @param options - Registry and metadata used for constructing the method.
+ */
+export function setKeys(
+    args: SessionSetKeysArgs,
+    info: BaseTxInfo,
+    options: OptionsWithMeta
+): UnsignedTransaction {
+    const { keys, proof = '0x' } = args;
+
+    return defineMethod(
+        {
+            method: {
+                args: {
+                    keys,
+                    proof,
+                },
+                name: 'setKeys',
+                pallet: 'session',
+            },
+            ...info,
+        },
+        options
+    );
+}
