4e86d4feb4d86b7741c6d9d0ae2301f754dc1c59
tarikgul <tarik@parity.io>
2021-02-06 23:56:46 -0500

    feat: export system and session methods

--- packages/txwrapper-substrate/src/methods/index.ts
+++ packages/txwrapper-substrate/src/methods/index.ts
@@ -2,6 +2,8 @@
 export * as balances from './balances';
 export * as democracy from './democracy';
 export * as proxy from './proxy';
+export * as session from './session';
 export * as staking from './staking';
+export * as system from './system';
 export * as utility from './utility';
 export * as vesting from './vesting';
