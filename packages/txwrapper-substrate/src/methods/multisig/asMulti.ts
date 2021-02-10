dbb050b8c83a537018532e6a6ebd869fc9781f98
emostov <32168567+emostov@users.noreply.github.com>
2021-02-09 16:32:16 -0800

    Update docs comments

--- packages/txwrapper-substrate/src/methods/multisig/asMulti.ts
+++ packages/txwrapper-substrate/src/methods/multisig/asMulti.ts
@@ -19,6 +19,27 @@ export interface MultisigAsMulti
 	storeCall: boolean;
 }
 
+/**
+ * Register approval for a dispatch to be made from a deterministic composite account if
+ * approved by a total of `threshold - 1` of `other_signatories`.
+ *
+ * If there are enough, then dispatch the call.
+ *
+ * Payment: `DepositBase` will be reserved if this is the first approval, plus
+ * `threshold` times `DepositFactor`. It is returned once this dispatch happens or
+ * is cancelled.
+ *
+ * NOTE: Unless this is the final approval, you will generally want to use
+ * `approve_as_multi` instead, since it only requires a hash of the call.
+ *
+ * Result is equivalent to the dispatched result if `threshold` is exactly `1`. Otherwise
+ * on success, result is `Ok` and the result from the interior call, if it was executed,
+ * may be found in the deposited `MultisigExecuted` event.
+ *
+ * @param args - Arguments specific to this method.
+ * @param info - Information required to construct the transaction.
+ * @param options - Registry and metadata used for constructing the method.
+ */
 export function asMulti(
 	args: MultisigAsMulti,
 	info: BaseTxInfo,
