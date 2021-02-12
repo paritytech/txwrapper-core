aa5a70806b3e3ec68a58137c06571be3f3a021f1
emostov <32168567+emostov@users.noreply.github.com>
2021-02-12 12:17:33 -0800

    Update Timepoint docs

--- packages/txwrapper-substrate/src/methods/multisig/types.ts
+++ packages/txwrapper-substrate/src/methods/multisig/types.ts
@@ -1,7 +1,7 @@
 /**
  * A global extrinsic index, formed as the extrinsic index within a block, together with that
  * block's height. This allows a transaction in which a multisig operation of a particular
- * composite was created to be uniquely identified.
+ * multisig account was created to be uniquely identified.
  */
 export type Timepoint = {
 	/**
