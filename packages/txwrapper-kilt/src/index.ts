9bd70afea164e3a47297884abde8f4c807b2ed1d
dudleyneedham <dudleyneedham1993@gmail.com>
2021-04-01 13:45:37 +0200

    feat: updating script and typing

--- packages/txwrapper-kilt/src/index.ts
+++ packages/txwrapper-kilt/src/index.ts
@@ -48,7 +48,7 @@ export function getRegistry({
 		// If your types are not packaged in the `OverrideBundleType` format, you can
 		// specify types in any of the formats supported by `RegisteredTypes`:
 		// https://github.com/polkadot-js/api/blob/4ff9b51af2c49294c676cc80abc6476565c70b11/packages/types/src/types/registry.ts#L59
-		typesBundle: kiltDefinitions as OverrideBundleType,
+		typesBundle: (kiltDefinitions as unknown) as OverrideBundleType,
 	});
 
 	return getRegistryBase({
