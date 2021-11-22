0545ca8cdd1a3982096a851207e457369402dfda
tarikgul <tarik@parity.io>
2021-11-22 10:05:41 -0500

    adds test to check if asCallsOnly returns a smaller metadata hex

---
+++ packages/txwrapper-core/src/core/metadata/createMetadata.spec.ts
@@ -0,0 +1,28 @@
+import { Metadata } from '@polkadot/types';
+import { MetadataVersioned } from '@polkadot/types/metadata/MetadataVersioned';
+
+import { polkadotV9122MetadataHex } from '../../test-helpers/metadata/polkadotV9122MetadataHex';
+import { polkadotRegistryV9122 } from '../../test-helpers/registries';
+import { createMetadataUnmemoized } from './createMetadata';
+
+describe('createMetadata', () => {
+	const metadata: Metadata = createMetadataUnmemoized(
+		polkadotRegistryV9122,
+		polkadotV9122MetadataHex
+	);
+	const metadataAsCalls: MetadataVersioned = createMetadataUnmemoized(
+		polkadotRegistryV9122,
+		polkadotV9122MetadataHex,
+		true
+	);
+
+	it('Metadata should decrease in byte size when `asCallsOnlyArg is true`', async () => {
+		const metadataBuffer = Buffer.from(metadata.toHex(), 'utf-8');
+		const metadataAsCallsBuffer = Buffer.from(metadataAsCalls.toHex(), 'utf-8');
+
+		expect(metadataAsCallsBuffer.byteLength).toBeGreaterThan(0);
+		expect(metadataBuffer.byteLength).toBeGreaterThan(
+			metadataAsCallsBuffer.byteLength
+		);
+	});
+});
