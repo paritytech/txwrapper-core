f74e403be42e785a2b8986bd1b357c7c964abeb5
tarikgul <tarik@parity.io>
2022-01-10 11:13:49 -0500

    export decode types for decodeEncodedUnsignedTx

--- packages/txwrapper-core/src/types/decode.ts
+++ packages/txwrapper-core/src/types/decode.ts
@@ -13,3 +13,21 @@ export type DecodedSignedTx = Omit<
 	| 'transactionVersion'
 	| 'version'
 >;
+
+/**
+ * This Decode type is not included as a return type for the standard `txwrapper-core`
+ * `decode` utility function. Instead this is specific to `decodeEncodedUnsignedTx`,
+ * which is the counterpart to `encodeUnsignedTx`.
+ */
+export interface DecodedEncodedUnsignedTx {
+	method: IMethod;
+	version: number;
+}
+
+/**
+ * Primitive version of `DecodeMethodInput` interface from polkadot-js.
+ */
+export interface IMethod {
+	args: unknown;
+	callIndex: string;
+}
