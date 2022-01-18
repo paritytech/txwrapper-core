fe72afdac448565f0913b568793ad0c82eaa0e57
tarikgul <tarik@parity.io>
2022-01-18 12:36:51 -0500

    fix(bud): remove enum from test-helpers, and into util

--- packages/txwrapper-core/src/core/util/deriveAddress.spec.ts
+++ packages/txwrapper-core/src/core/util/deriveAddress.spec.ts
@@ -1,4 +1,4 @@
-import { PolkadotSS58Format } from '../../test-helpers';
+import { PolkadotSS58Format } from '../util';
 import { deriveAddress } from './deriveAddress';
 
 describe('deriveAddress', () => {
--- packages/txwrapper-core/src/test-helpers/constants.ts
+++ packages/txwrapper-core/src/test-helpers/constants.ts
@@ -84,14 +84,3 @@ export const TEST_METHOD_ARGS = {
 		},
 	},
 };
-
-/**
- * Prefix for ss58-encoded addresses on Polkadot, Kusama, and Westend. Note:
- * 42, the Westend prefix, is also the default for Substrate-based chains.
- */
-export enum PolkadotSS58Format {
-	polkadot = 0,
-	kusama = 2,
-	westend = 42,
-	substrate = 42,
-}
