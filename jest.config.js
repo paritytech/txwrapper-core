1a42a588cd615d0dbe542938994acca15d04d945
José <jarcodallo@gmail.com>
2022-02-22 09:59:17 -0300

    ci: improve Pipeline (#182)
    * Improve Pipeline
    
    * remove wasm files

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,16 @@ const base = require('@substrate/dev/config/jest');
 
 module.exports = {
 	...base,
+	verbose: true,
+	coverageThreshold: {
+		global: {
+			branches: 50,
+			functions: 70,
+			lines: 70,
+			statements: 85
+		}
+	},
+	testEnvironment: 'node',
 	testPathIgnorePatterns: ['lib', 'node_modules'],
 	moduleNameMapper: pathsToModuleNameMapper(paths, {
 		prefix: '<rootDir>/packages',
