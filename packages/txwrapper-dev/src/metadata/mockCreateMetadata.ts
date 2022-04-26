f497029ede3b405c803f5c23b9825d89fbba8f9d
tarikgul <tarik@parity.io>
2022-04-26 16:13:39 -0400

    correctly structure the mock metadata folder

---
+++ packages/txwrapper-dev/src/metadata/mockCreateMetadata.ts
@@ -0,0 +1,37 @@
+import { Metadata } from '@polkadot/types';
+import { TypeRegistry } from '@polkadot/types';
+import { MetadataVersioned } from '@polkadot/types/metadata/MetadataVersioned';
+import memoizee from 'memoizee';
+
+/**
+ * From a metadata hex string (for example returned by RPC), create a Metadata
+ * class instance. Metadata decoding is expensive, so this function is
+ * memoized.
+ *
+ * @ignore
+ * @param registry - The registry of the metadata.
+ * @param metadata - The metadata as hex string.
+ * @param asCallsOnlyArg - Option to decreases the metadata to calls only
+ */
+export function mockCreateMetadataUnmemoized(
+    registry: TypeRegistry,
+    metadataRpc: `0x${string}`,
+    asCallsOnlyArg = false
+): Metadata | MetadataVersioned {
+    const metadata = new Metadata(registry, metadataRpc);
+    return asCallsOnlyArg ? metadata.asCallsOnly : metadata;
+}
+
+/**
+ * From a metadata hex string (for example returned by RPC), create a Metadata
+ * class instance. Metadata decoding is expensive, so this function is
+ * memoized.
+ *
+ * @ignore
+ * @param registry - The registry of the metadata.
+ * @param metadata - The metadata as hex string.
+ * @param asCallsOnlyArg - Option to decreases the metadata to calls only
+ */
+export const mockCreateMetadata = memoizee(mockCreateMetadataUnmemoized, {
+    length: 3,
+});
