f27e72f490bd229ea7c18b538dba3a03c730a67c
tarikgul <tarik@parity.io>
2022-06-21 22:22:42 -0400

    feat: xcm pallet transaction boilerplate

---
+++ packages/txwrapper-polkadot/src/methods/xcmPallet/teleportAssets.ts
@@ -0,0 +1,59 @@
+import {
+	Args,
+	BaseTxInfo,
+	defineMethod,
+	OptionsWithMeta,
+	UnsignedTransaction,
+} from '@substrate/txwrapper-core';
+
+export interface XcmTeleportAssets extends Args {
+    /**
+     * Destination context for the assets. Will typically be `X2(Parent, Parachain(..))` to send
+	 * from parachain to parachain, or `X1(Parachain(..))` to send from relay to parachain.
+     */
+     dest: string;
+     /**
+      * A beneficiary location for the assets in the context of `dest`. Will generally be
+      * an `AccountId32` value.
+      */
+     beneficiary: string;
+     /**
+      * The assets to be withdrawn. This should include the assets used to pay the fee on the
+      * `dest` side.
+      */
+     assets: string[];
+     /**
+      * The index into `assets` of the item which should be used to pay
+      * fees.
+      */
+     feeAssetsItem: number;
+}
+
+/**
+ * Teleport some assets from the local chain to some destination chain.
+ *
+ * Fee payment on the destination side is made from the asset in the `assets` vector of
+ * index `fee_asset_item`. The weight limit for fees is not provided and thus is unlimited,
+ * with all fees taken as needed from the asset.
+ * 
+ * @param args - Arguments specific to this method.
+ * @param info - Information required to construct the transaction.
+ * @param options - Registry and metadata used for constructing the method. 
+ */
+export function TeleportAssets(
+    args: XcmTeleportAssets,
+	info: BaseTxInfo,
+	options: OptionsWithMeta
+): UnsignedTransaction {
+    return defineMethod(
+        {
+			method: {
+				args,
+				name: 'reserveTransferAssets',
+				pallet: 'xcm-pallet',
+			},
+			...info,
+		},
+		options
+    )
+}
