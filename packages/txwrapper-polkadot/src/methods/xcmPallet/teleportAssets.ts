b5e5bef640d0d34383dd2ab1f3cf681cbf0aec76
tarikgul <tarik@parity.io>
2022-08-15 11:28:50 -0400

    cleanup types

--- packages/txwrapper-polkadot/src/methods/xcmPallet/teleportAssets.ts
+++ packages/txwrapper-polkadot/src/methods/xcmPallet/teleportAssets.ts
@@ -1,34 +1,13 @@
-import { AnyJson } from '@polkadot/types/types';
 import {
-	Args,
 	BaseTxInfo,
 	defineMethod,
 	OptionsWithMeta,
 	UnsignedTransaction,
 } from '@substrate/txwrapper-core';
 
-export interface XcmTeleportAssets extends Args {
-	/**
-	 * Destination context for the assets. Will typically be `X2(Parent, Parachain(..))` to send
-	 * from parachain to parachain, or `X1(Parachain(..))` to send from relay to parachain.
-	 */
-	dest: AnyJson;
-	/**
-	 * A beneficiary location for the assets in the context of `dest`. Will generally be
-	 * an `AccountId32` value.
-	 */
-	beneficiary: AnyJson;
-	/**
-	 * The assets to be withdrawn. This should include the assets used to pay the fee on the
-	 * `dest` side.
-	 */
-	assets: AnyJson;
-	/**
-	 * The index into `assets` of the item which should be used to pay
-	 * fees.
-	 */
-	feeAssetItem: number;
-}
+import { XcmBaseAssetArgs } from './types';
+
+export type XcmTeleportAssets = XcmBaseAssetArgs;
 
 /**
  * Teleport some assets from the local chain to some destination chain.
