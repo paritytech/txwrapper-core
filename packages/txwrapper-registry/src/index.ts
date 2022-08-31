12a13d783e0a2c6059ea275195fe6491545f5853
tarikgul <tarik@parity.io>
2022-08-31 18:46:16 -0400

    rename env var to TX_TYPES_BUNDLE

--- packages/txwrapper-registry/src/index.ts
+++ packages/txwrapper-registry/src/index.ts
@@ -48,8 +48,8 @@ export interface GetRegistryOpts extends GetRegistryOptsCore {
 	properties?: ChainProperties;
 }
 
-const typesBundle: OverrideBundleType = process.env.SUBSTRATE_TYPES_BUNDLE
-	? require(process.env.SUBSTRATE_TYPES_BUNDLE)
+const typesBundle: OverrideBundleType = process.env.TX_TYPES_BUNDLE
+	? require(process.env.TX_TYPES_BUNDLE)
 	: undefined;
 
 /**
