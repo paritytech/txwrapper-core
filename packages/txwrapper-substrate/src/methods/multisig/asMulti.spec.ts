6c32f05480b0d869fd9a6955010027e7e172630f
tarikgul <tarik@parity.io>
2022-11-30 11:26:00 -0500

    fix tests for multisig transactions

--- packages/txwrapper-substrate/src/methods/multisig/asMulti.spec.ts
+++ packages/txwrapper-substrate/src/methods/multisig/asMulti.spec.ts
@@ -1,6 +1,6 @@
 import {
 	itHasCorrectBaseTxInfo,
-	POLKADOT_25_TEST_OPTIONS,
+	KUSAMA_9320_TEST_OPTIONS,
 	TEST_BASE_TX_INFO,
 } from '@substrate/txwrapper-dev';
 
@@ -13,13 +13,13 @@ describe('multisig::asMulti', () => {
 		const unsigned = asMulti(
 			TEST_METHOD_ARGS.multisig.asMulti,
 			TEST_BASE_TX_INFO,
-			POLKADOT_25_TEST_OPTIONS
+			KUSAMA_9320_TEST_OPTIONS
 		);
 
 		itHasCorrectBaseTxInfo(unsigned);
 
 		expect(unsigned.method).toBe(
-			'0x1e0102000cd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a4890b5ab205c6974c9ea841be688864633dc9ca8a357843eeacf2314649965fe22017b00000003000000a80500306721211d5404bd9da88e0204360a1a9ab8b87c66c1bc2fcdd37f3c2222cc200f00a0be1c44839900f6ffffffffff3f01'
+			'0x1f0102000cd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a4890b5ab205c6974c9ea841be688864633dc9ca8a357843eeacf2314649965fe22017b00000003000000040300cc5fc7439d3c49984e7345319de148561cd930748fb401276d289f714acdd50c2813f6ffffffffff3f0100'
 		);
 	});
 
@@ -27,7 +27,7 @@ describe('multisig::asMulti', () => {
 		const unsignedBalancesTransferKeepAlive = transferKeepAlive(
 			TEST_METHOD_ARGS.balances.transferKeepAlive,
 			TEST_BASE_TX_INFO,
-			POLKADOT_25_TEST_OPTIONS
+			KUSAMA_9320_TEST_OPTIONS
 		);
 
 		const unsignedAsMulti = asMulti(
@@ -36,13 +36,13 @@ describe('multisig::asMulti', () => {
 				call: unsignedBalancesTransferKeepAlive.method,
 			},
 			TEST_BASE_TX_INFO,
-			POLKADOT_25_TEST_OPTIONS
+			KUSAMA_9320_TEST_OPTIONS
 		);
 
 		itHasCorrectBaseTxInfo(unsignedAsMulti);
 
 		expect(unsignedAsMulti.method).toBe(
-			'0x1e0102000cd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a4890b5ab205c6974c9ea841be688864633dc9ca8a357843eeacf2314649965fe22017b000000030000008c060396074594cccf1cd185fa8a72ceaeefd86648f8d45514f3ce33c31bdd07e4655d3000f6ffffffffff3f01'
+			'0x1f0102000cd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a4890b5ab205c6974c9ea841be688864633dc9ca8a357843eeacf2314649965fe22017b0000000300000004030096074594cccf1cd185fa8a72ceaeefd86648f8d45514f3ce33c31bdd07e4655d3013f6ffffffffff3f0100'
 		);
 	});
 });
