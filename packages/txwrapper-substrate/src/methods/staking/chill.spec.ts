0b5036cedb0a75f392c07d54eee5f5751f5563c9
tarikgul <tarik@parity.io>
2023-05-03 22:01:38 -0400

    staking

--- packages/txwrapper-substrate/src/methods/staking/chill.spec.ts
+++ packages/txwrapper-substrate/src/methods/staking/chill.spec.ts
@@ -1,6 +1,6 @@
 import {
 	itHasCorrectBaseTxInfo,
-	POLKADOT_25_TEST_OPTIONS,
+	KUSAMA_TEST_OPTIONS,
 	TEST_BASE_TX_INFO,
 } from '@substrate/txwrapper-dev';
 
@@ -12,10 +12,10 @@ describe('staking::chill', () => {
 		const unsigned = chill(
 			TEST_METHOD_ARGS.staking.chill,
 			TEST_BASE_TX_INFO,
-			POLKADOT_25_TEST_OPTIONS
+			KUSAMA_TEST_OPTIONS
 		);
 
 		itHasCorrectBaseTxInfo(unsigned);
-		expect(unsigned.method).toBe('0x0806');
+		expect(unsigned.method).toBe('0x0606');
 	});
 });
--- packages/txwrapper-substrate/src/methods/staking/withdrawUnbonded.spec.ts
+++ packages/txwrapper-substrate/src/methods/staking/withdrawUnbonded.spec.ts
@@ -1,6 +1,6 @@
 import {
 	itHasCorrectBaseTxInfo,
-	POLKADOT_25_TEST_OPTIONS,
+	KUSAMA_TEST_OPTIONS,
 	TEST_BASE_TX_INFO,
 } from '@substrate/txwrapper-dev';
 
@@ -12,10 +12,10 @@ describe('staking::withdrawUnbonded', () => {
 		const unsigned = withdrawUnbonded(
 			TEST_METHOD_ARGS.staking.withdrawUnbonded,
 			TEST_BASE_TX_INFO,
-			POLKADOT_25_TEST_OPTIONS
+			KUSAMA_TEST_OPTIONS
 		);
 
 		itHasCorrectBaseTxInfo(unsigned);
-		expect(unsigned.method).toBe('0x08030b000000');
+		expect(unsigned.method).toBe('0x06030b000000');
 	});
 });
