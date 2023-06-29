89071d45e4905a9ab60577456ef2406f1d122093
tarikgul <tarik@parity.io>
2023-06-29 06:32:53 -0400

    fix up tests

--- packages/txwrapper-core/src/core/metadata/toSpecifiedCallsOnlyV14.spec.ts
+++ packages/txwrapper-core/src/core/metadata/toSpecifiedCallsOnlyV14.spec.ts
@@ -1,20 +1,20 @@
 import {
-	KUSAMA_SPEC_VERSION,
-	kusamaMetadataHex,
-	memoizedKusamaGetRegistry,
+	memoizedPolkadotGetRegistry,
 	mockCreateMetadata,
+	POLKADOT_SPEC_VERSION,
+	polkadotMetadataHex,
 } from '@substrate/txwrapper-dev';
 
 import mockToSpecifiedCallsV14 from '../../test-helpers/mock/toSpecifiedCallsOnlyV14.json';
 import { toSpecifiedCallsOnlyV14 } from './';
 
 describe('toSpecifiedCallsOnly', () => {
-	const registry = memoizedKusamaGetRegistry(
-		KUSAMA_SPEC_VERSION,
-		kusamaMetadataHex
+	const registry = memoizedPolkadotGetRegistry(
+		POLKADOT_SPEC_VERSION,
+		polkadotMetadataHex
 	);
 	it('Should correctly parse the metadata', () => {
-		const metadata = mockCreateMetadata(registry, kusamaMetadataHex);
+		const metadata = mockCreateMetadata(registry, polkadotMetadataHex);
 		const callsOnly = toSpecifiedCallsOnlyV14(registry, metadata.asLatest, [
 			'balances',
 			'system',
