e392a2d666a1740f71bc4568bd1e8d2f268083ed
Alberto Nicolas Penayo <74352651+bee344@users.noreply.github.com>
2024-08-21 08:07:15 -0300

    fix: set assetId to default to undefined instead of 0 (#397)

--- packages/txwrapper-core/src/core/decode/decodeUnsignedTx.ts
+++ packages/txwrapper-core/src/core/decode/decodeUnsignedTx.ts
@@ -53,8 +53,9 @@ export function decodeUnsignedTx(
 	}
 	return {
 		address: unsigned.address,
-		assetId:
-			typeof assetId === 'object'
+		assetId: !assetId
+			? undefined
+			: typeof assetId === 'object'
 				? registry.createType('MultiLocation', assetId)
 				: registry.createType('Compact<AssetId>', assetId).toNumber(),
 		blockHash: unsigned.blockHash,
